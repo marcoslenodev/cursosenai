3af46a8b68aac3d22938cba432642bb0ce93b930
marcoslenodev <157828124+marcoslenodev@users.noreply.github.com>
2024-01-28 15:24:42 -0300

    Add files via upload

---
+++ atividade_12_SP2_Codificacao_Tela_Cadastro/ProjetoFilmes/Model/FilmesTableViewCell.swift
@@ -0,0 +1,11 @@
+
+
+import UIKit
+
+class FilmesTableViewCell: UITableViewCell{
+    
+    @IBOutlet weak var capaImageView: UIImageView!
+    @IBOutlet weak var tituloLabel: UILabel!
+    @IBOutlet weak var generoLabel: UILabel!
+    @IBOutlet weak var diretorLabel: UILabel!
+}
